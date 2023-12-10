783c033953ffe246eb4f61655d62d9f0ab257089
schizolab <schizolab@yeah.net>
2023-12-11 02:42:12 +0800

    Scaffolding

--- index.js
+++ index.js
@@ -3,8 +3,37 @@ import { Command } from "commander";
 const program = new Command()
 
 program
-  .name('ffmpeg cluster')
-  .description('The cluster node for transcoding videos to vp9, this needs to connect to the master')
-  .version('0.2.0')
+    .name('ffmpeg cluster')
+    .description('The cluster node for transcoding videos to vp9, this needs to connect to the master')
+    .version('0.2.0')
 
-await program.parseAsync()
+program
+    .command('start')
+    .argument('<master address>', 'The master address')
+    .argument('[threads]', 'How many parallel threads to use for encoding')
+    .description('Connect to the master and start processing')
+    .action((masterAddress, threads = 4) => {
+        // principle of operation
+        {
+            // connect to master
+
+            // set name in socket
+
+            // parallel processing
+            {
+                // fetch task
+
+                // fetch file from URL
+                // report to socket along the way
+
+                // process with ffmpeg
+                // report to socket along the way
+
+                // upload to URL
+
+                // report task
+            }
+        }
+    })
+
+program.parse()
