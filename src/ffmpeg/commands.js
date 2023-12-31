f92d648a0d837caa90b24ee35e2a564c204783b7
schizolab <schizolab@yeah.net>
2023-12-31 13:19:41 +0800

    Adding support for hevc_videotoolbox. Need to tune quality, looks really bad.

--- src/ffmpeg/commands.js
+++ src/ffmpeg/commands.js
@@ -1,7 +1,7 @@
 const CPU_USED = 1
 
 export function getOutputExtension() {
-    switch (process.env.HW_RENDERER) {
+    switch (process.env.HW_ENCODER) {
         case 'videotoolbox':
             return '.mp4'
         case 'nvenc':
@@ -12,9 +12,26 @@ export function getOutputExtension() {
 }
 
 export function getFFMPEGCommand({ inputFilePath, isDeNoise, quality, width, outputFilePath }) {
-    switch (process.env.HW_RENDERER) {
+    switch (process.env.HW_ENCODER) {
+        // appleshit
+        // I have a Macbook Air M2 as a normie machine
+        // to leave totally innocuous browsing history to the big tech
+        // which I believe is important for the future
         case 'videotoolbox':
-
+            console.log('using videotoolbox')
+            return `ffmpeg -i ${inputFilePath} \
+            -c:v hevc_videotoolbox \
+            -c:a libopus \
+            -b:v 0 ${isDeNoise ? '-vf "hqdn3d=4:3:6:4" ' : ''}\
+            -crf ${quality} \
+            -q:v 2 \
+            -auto-alt-ref 1 \
+            -lag-in-frames 25 \
+            -vf scale=${width}:-2 \
+            -progress pipe:1 \
+            -hide_banner \
+            -loglevel error \
+            ${outputFilePath}`
             break;
         case 'nvenc':
 
